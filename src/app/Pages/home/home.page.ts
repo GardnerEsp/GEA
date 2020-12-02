2754e85c0815285a0b55af1248972bb6d7293524
Marco Alejandro Marcial Coronado <marco.marcial@pseinternship.com>
2020-12-02 12:23:00 -0700

    Markers in map

--- src/app/Pages/home/home.page.ts
+++ src/app/Pages/home/home.page.ts
@@ -1,70 +1,84 @@
-import { Component,OnInit  } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { MenuController } from '@ionic/angular';
 import { ServicesService } from '../../Services/services.service';
 import * as mapboxgl from 'mapbox-gl';
 import { LngLatLike } from 'mapbox-gl';
 import { environment } from '@env/environment';
+import { GeoJson, FeatureCollection } from '../../Models/mapJson.model';
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })
-export class HomePage  implements OnInit  {
+export class HomePage implements OnInit {
 
-  intervale:any;
+  intervale: any;
+  markers: any;
+
   mapbox = (mapboxgl as typeof mapboxgl);
   map: mapboxgl.Map;
-  constructor(private mapService:ServicesService, 
-              private router:Router,
-              private menu:MenuController) {
-              }
-  
+  style = `mapbox://styles/mapbox/streets-v11`;
+  lat = 43.1746;
+  lng = -2.4125;
+  zoom = 15;
+
+  constructor(
+    private mapService: ServicesService,
+    private router: Router,
+    private menu: MenuController) { }
+
   ngOnInit() {
     this.ionViewDidEnter();
-    this.menu.enable(true,"first");
-
+    this.menu.enable(true, "first");
   }
-  
+
   ionViewDidEnter() {
-    this.mapService.buildMap();
+    this.buildMap();
     this.intervale = setTimeout(() => {
       this.uploadMarkersToMap();
-    }, 1500);
+    }, 1000);
   }
 
-  
-  toggleMenu(){
-    this.menu.toggle();
+  buildMap() {
+    this.mapbox.accessToken = environment.mapBoxToken;
+    this.map = new mapboxgl.Map({
+      container: 'map',
+      style: this.style,
+      center: [-109.9459475, 27.5432134],
+      zoom: 12,
+    });
+    this.map.addControl(new mapboxgl.NavigationControl());
+    this.map.resize();
   }
-  createMarkerMapIcon(marker): HTMLElement {
-    let markerIcon = document.createElement('i');
-    markerIcon.style.fontSize = "401px";
-    markerIcon.style.color = marker.geometry.marker.category.color;
-    markerIcon.className = 'fas fa-map-marker-alt';   
-    return markerIcon;
+
+  toggleMenu() {
+    this.menu.toggle();
   }
 
   private uploadMarkersToMap(): void {
-    this.mapService.getGeoJsonMarkers().features.forEach((marker, index) => {
-      if(index > 0){
+    this.mapService.getMarkers().forEach((marker, index) => {
         const markerIcon = this.createMarkerMapIcon(marker);
         markerIcon.addEventListener('click', () => {
           this.map.flyTo({
             zoom: 12.5,
-            center: [
-              marker.geometry.marker.latitude,
-              marker.geometry.marker.longitude] as LngLatLike
+            center: marker.geometry.coordinates as LngLatLike
           });
         });
+        
         new mapboxgl.Marker(markerIcon)
-          .setLngLat([
-            marker.geometry.marker.latitude,
-            marker.geometry.marker.longitude] as LngLatLike)
+          .setLngLat( marker.geometry.coordinates as LngLatLike)
           .addTo(this.map);
-      }
     });
   }
-  
+
+  createMarkerMapIcon(marker): HTMLElement {
+    let markerIcon = document.createElement('ion-icon');
+    markerIcon.name = "pin-sharp"
+    markerIcon.style.fontSize = "40px";
+    markerIcon.style.color = marker.properties.color;
+    return markerIcon;
+  }
+
 }
