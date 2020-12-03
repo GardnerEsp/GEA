e923b6c4afc0014cdba8bb42dd4368ddd206e4fd
Marco Alejandro Marcial Coronado <marco.marcial@pseinternship.com>
2020-12-02 22:16:01 -0700

    Split map logic

--- src/app/Models/category.model.ts
+++ src/app/Models/category.model.ts
@@ -1,5 +1,6 @@
 export interface Category{
-    id: string,
+    id: string
     name: string
     color: string
+    iconName: string
 }
--- src/app/Pages/home/home.page.ts
+++ src/app/Pages/home/home.page.ts
@@ -1,14 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { MenuController } from '@ionic/angular';
-import { MapComponent } from '../../components/map/map.component';
-
-import * as mapboxgl from 'mapbox-gl';
-import { LngLatLike } from 'mapbox-gl';
-import { environment } from '@env/environment';
-import { GeoJson, FeatureCollection } from '../../Models/mapJson.model';
-import { ServicesService } from 'src/app/Services/services.service';
-
 
 @Component({
   selector: 'app-home',
@@ -17,26 +9,12 @@ import { ServicesService } from 'src/app/Services/services.service';
 })
 export class HomePage implements OnInit {
 
-
-  intervale: any;
-  markers: any;
-
-  mapbox = (mapboxgl as typeof mapboxgl);
-  map: mapboxgl.Map;
-  style = `mapbox://styles/mapbox/streets-v11`;
-  lat = 43.1746;
-  lng = -2.4125;
-  zoom = 15;
-
-
   constructor(
     private router: Router,
-    private menu: MenuController,
-    private mapService: ServicesService) { 
+    private menu: MenuController) { 
     }
 
   ngOnInit() {
-    this.ionViewDidEnter();
     this.menu.enable(true, "first");
   }
 
@@ -46,51 +24,5 @@ export class HomePage implements OnInit {
   addReport(){
     this.router.navigateByUrl('/add-report');
   }
-
-  ionViewDidEnter() {
-    this.buildMap();
-    this.intervale = setTimeout(() => {
-      this.uploadMarkersToMap();
-    }, 1000);
-  }
-
-  buildMap() {
-    this.mapbox.accessToken = environment.mapBoxToken;
-    this.map = new mapboxgl.Map({
-      container: 'map',
-      style: this.style,
-      center: [-109.9459475, 27.5432134],
-      zoom: 12,
-    });
-    this.map.addControl(new mapboxgl.NavigationControl());
-    this.map.resize();
-  }
-
-  
-
-  private uploadMarkersToMap(): void {
-    this.mapService.getMarkers().forEach((marker, index) => {
-        const markerIcon = this.createMarkerMapIcon(marker);
-        markerIcon.addEventListener('click', () => {
-          this.map.flyTo({
-            zoom: 12.5,
-            center: marker.geometry.coordinates as LngLatLike
-          });
-        });
-        
-        new mapboxgl.Marker(markerIcon)
-          .setLngLat( marker.geometry.coordinates as LngLatLike)
-          .addTo(this.map);
-    });
-  }
-
-  createMarkerMapIcon(marker): HTMLElement {
-    let markerIcon = document.createElement('ion-icon');
-    markerIcon.name = "pin-sharp"
-    markerIcon.style.fontSize = "40px";
-    markerIcon.style.color = marker.properties.color;
-    return markerIcon;
-  }
-
 
 }
